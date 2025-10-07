573b6e0b1ce04417422e76948670c514bb6db447
Rodolphe Jacson <rodolphe.jacson@gmail.com>
2025-10-08 00:11:18 +0200

    feat: add developer card demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import CardsDemo from '../pages/cards-demo.tsx';
 import './styles/variables.css';
 import './styles/globals.css';
 
+const normalizedPath =
+  window.location.pathname.replace(/\/+$/, '') || '/';
+const isCardsDemo = normalizedPath === '/cards-demo';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isCardsDemo ? <CardsDemo /> : <App />}
   </React.StrictMode>
 );
